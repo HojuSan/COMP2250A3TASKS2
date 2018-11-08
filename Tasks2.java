f4db932dd59cd6ab2af8a646061e3d8a8ed8cdd5
HojuSan <juyong.kim@uon.edu.au>
2018-11-08 12:03:14 +1100

    got first sections to run

--- Tasks2.java
+++ Tasks2.java
@@ -57,167 +57,195 @@ public class Tasks2
 //        int e = generateE(m);
 //        int d = generateD(e,n);
         BigInteger newP = generatePrime();
-        BigInteger newG = generateG(newP);
-        BigInteger n = newP.multiply(newG);
-        //for calculation purposes
-        int newP1 = newP.intValue()-1;
-        int newG1 = newG.intValue()-1;
+        System.out.println("newP " + newP);
+//        BigInteger newG = generateG(newP);
+        BigInteger newQ = generatePrime();
+        System.out.println("\nnewQ "+ newQ);
+        BigInteger newG = generatePrime();
+        System.out.println("\nnewG "+ newG);
+        BigInteger n = newP.multiply(newQ);
+        System.out.println("\nn "+n);
 
-        BigInteger m = BigInteger.valueOf(newP1).multiply(BigInteger.valueOf(newG1));
-//TODO need to work on this section
+        //m=(p-1)(q-1)
+        BigInteger m = (newP.subtract(BigInteger.ONE)).multiply(newQ.subtract(BigInteger.ONE));
+        System.out.println("\nm "+m);
         BigInteger e = generateE(m);
+        System.out.println("\ne "+e);
         BigInteger d = generateD(e,n);
+        System.out.println("\nd "+d);
 
+        BigInteger BobPrivate=generatePrivateKey(newP);
+        BigInteger AlicePrivate=generatePrivateKey(newP);
+        System.out.println("\nBob generates private key "+BobPrivate);
+        System.out.println("\nAlice generates private key "+AlicePrivate);
 
-        System.out.println("An asymmetric signature keypair for each party has been generated\n");
-        System.out.println("Safe prime and Generator has been created as well\n");
+        BigInteger BobPublic=generatePublicKey(newG,BobPrivate,newP);
+        BigInteger AlicePublic=generatePublicKey(newG,AlicePrivate,newP);
+        System.out.println("\nBob generates public key "+BobPublic);
+        System.out.println("\nAlice generates public key "+AlicePublic);
 
-        System.out.println("prime " + newP+"\n");
-        System.out.println("generator " + newG+"\n");
-
-        System.out.println("Alice generates a random number x: ");
+        System.out.println("\nAlice generates a random number x: ");
         int x = sc.nextInt();
-        System.out.println("then send g^x to Bob\n");
+        System.out.println("\nthen send g^x to Bob\n");
 
-        System.out.println("Bob generates a random number y: ");
+        System.out.println("\nBob generates a random number y: ");
         int y = sc.nextInt();
 
-
-        //K=(g^x)^y
-        int BobK = computeExp(computeExp(newG,x),y);
-        System.out.println("Bob computates the shared key K=(g^x)^y: "+BobK);
-
-//        int biGX = Integer.toBinaryString(computeExp(newG,x));
-//        int biGY = Integer.toBinaryString(computeExp(newG,y));
-
-        System.out.println("xbinary: "+computeExp(newG,x));
-        System.out.println("\nybinary: "+computeExp(newG,y));
-
-        //concatenate binery or string?
-
-        //signs the concatenated string with his private key
-
-        //encrypts the signature with K
-
-        //sends cipher text with his g^y to Alice
-
-        //Alice then computes K=(g^y)^x
-
-
+        System.out.println("\nBob generates a random number g^y: "  );
 
 
     }
 
-    public static BigInteger generateD(BigInteger e,BigInteger n)
+    public static BigInteger generatePrivateKey(BigInteger num)
     {
-        int d;
-        d= (1%n)/e;
-        return d;
-    }
-    public static BigInteger generateE(BigInteger m)
-    {
-        Random rand = new Random();
-        int bigM = m;
-        int e = 0;
-        //while gcd isnt 1
-        while(true)
-        {
-            e = rand.nextInt(m) + 2;
-
-            if(findGCD(e,bigM)==1)
-            {
-                System.out.println("e is: "+e);
-                return e;
-            }
-        }
-    }
-    private static BigInteger findGCD(BigInteger number1, BigInteger number2) 
-    { 
-        //base case 
-        if(number2 == 0)
-        { 
-            return number1; 
-        } 
-        return findGCD(number2, number1%number2); 
-    }
-
-
-    public static int computeExp(int g, int num)
-    {
-        double expNum = Math.pow(g,num);
-        int newNum = (int) expNum;
-        return newNum;
-    }
-
-    public static BigInteger generateG(BigInteger prime)
-    {
-        BigInteger g;
-        int zero = 0;
+        BigInteger num2;
         while(true)
         {
             int bitLength= 1024;
             SecureRandom rnd2=new SecureRandom();
-            g=BigInteger.probablePrime(bitLength, rnd2).intValue();
+            num2 = new BigInteger(bitLength, rnd2);
 
-            if (prime.compareTo(g)==1 && g.intValue()>0)
+            if(num2.compareTo(num)==-1 && num2.compareTo(BigInteger.ONE)==1)
             {
-				return g;
+                return num2;
             }
         }
+    }
+    public static BigInteger generatePublicKey(BigInteger g, BigInteger privateKey, BigInteger p)
+    {
+        BigInteger num;
+        num = g.modPow(privateKey,p);
+        return num;
+    }
+    public static BigInteger generateD(BigInteger e,BigInteger n)
+    {
+        BigInteger d;
+
+        d = ((BigInteger.ONE.mod(n)).divide(e));
+
+        return d;
+    }
+
+    public static BigInteger generateE(BigInteger m)
+    {
+        BigInteger n = m.subtract(BigInteger.ONE);
+        BigInteger e;
+        int bits= 1024;
+        SecureRandom rnd1=new SecureRandom();
+        e = new BigInteger(bits, rnd1);
+
+        while(true)
+        {
+
+            int bitLength= 1024;
+            SecureRandom rnd2=new SecureRandom();
+            e = new BigInteger(bitLength, rnd2);
+
+//            System.out.println("gcd "+m.gcd(e)+"\nvalue of m "+ m +"\nvalue of e "+ e);
+
+            //gcd =1 and 1 < e < m-1
+            if(m.gcd(e).compareTo(BigInteger.ONE)==0 && e.compareTo(n)==-1 && e.compareTo(BigInteger.ONE)==1)
+            {
+                return e;
+            }
+        }
+        
+    }
+
+    public static BigInteger computeExp(BigInteger g, int num)
+    {
+        BigInteger newNum = g.pow(num);
+        return newNum;
     }
 
     public static BigInteger generatePrime()
     {
-		int safeP = 16;
-		/*7 is a known prime that doesn't generate a safe prime when*/
-		/*You plug it in, creates 15, so the while loops as long as a safe prime is not found*/
-		while(safeP != 7)
-		{
-            /*generates random prime numbers*/
-            SecureRandom rnd1=new SecureRandom();
-            int bits = 1024;
-			safeP =BigInteger.probablePrime(bits, rnd1).intValue();
-			
-			/*Creates a safe prime*/
-			safeP = 2*safeP+1;
-			
-			/*Checks if it is still a prime after calculations also has to be positive*/
-            if (isPrime(safeP) && safeP>0)
-            {
-                BigInteger num = BigInteger.valueOf(safeP);
-				return num;
-            }
-        }
-        //false news
-        return -1;
-
+        BigInteger p;
+        int bitLength= 1024;
+        SecureRandom rnd1=new SecureRandom();
+        p=BigInteger.probablePrime(bitLength, rnd1);
+        return p;
     }
 
-    public static boolean isPrime(int number) 
-    { 
-        //if 2 or 3 return true
-        if(number == 2 || number == 3) 
-        { 
-            return true; 
-        } 
-        //if even number
-        if(number % 2 == 0) 
-        { 
-            return false; 
-        } 
+//    public static BigInteger generateG(BigInteger prime)
+//    {
+//        BigInteger g;
+//        int zero = 0;
+//        while(true)
+//        {
+//            int bitLength= 1024;
+//            SecureRandom rnd2=new SecureRandom();
+//            g=BigInteger.probablePrime(bitLength, rnd2);
+//
+//            if (prime.compareTo(g)==1 && g.intValue()>0)
+//            {
+//				return g;
+//            }
+//        }
+//    }
 
-        int sqrt = (int) Math.sqrt(number) + 1; 
+//    public static BigInteger generatePrime()
+//    {
+//        BigInteger safeP;
+//        int two1 =2;
+//        BigInteger two = BigInteger.valueOf(two1);
+//		/*7 is a known prime that doesn't generate a safe prime when*/
+//		/*You plug it in, creates 15, so the while loops as long as a safe prime is not found*/
+//		while(true)
+//		{
+//            /*generates random prime numbers*/
+//            SecureRandom rnd1=new SecureRandom();
+//            int bits = 1024;
+//			safeP =BigInteger.probablePrime(bits, rnd1);
+//			
+//			/*Creates a safe prime*/
+//			safeP = (safeP.multiply(two)).add(BigInteger.ONE);
+//			
+//			/*Checks if it is still a prime after calculations also has to be positive*/
+//            if (isPrime(safeP))
+//            {
+//				return safeP;
+//            }
+//        }
+//
+//    }
 
-        for(int i = 3; i < sqrt; i += 2) 
-        { 
-            if (number % i == 0) 
-            { 
-                return false; 
-            } 
-        } 
-        //can't find so is true
-        return true; 
-    }
+//    public static boolean isPrime(BigInteger number) 
+//    { 
+//        int two1 =2;
+//        BigInteger two = BigInteger.valueOf(two1);
+//        int three1 =3;
+//        BigInteger three = BigInteger.valueOf(three1);
+//
+//        //if 2 or 3 return true
+//        if(number.compareTo(two) == 0 || number.compareTo(three) == 3) 
+//        { 
+//            return true; 
+//        } 
+//        //if even number
+//        if(number.mod(two)== 0) 
+//        { 
+//            return false; 
+//        } 
+//
+//        //int sqrt = (int) Math.sqrt(number) + 1; 
+////
+//        //for(int i = 3; i < sqrt; i += 2) 
+//        //{ 
+//        //    if (number % i == 0) 
+//        //    { 
+//        //        return false; 
+//        //    } 
+//        //} 
+//
+//        BigInteger sqrt = (number.pow(2)).add(BigInteger.ONE);
+//        for()
+//        {
+//            
+//        }
+//        //can't find so is true
+//        return true; 
+//    }
 
 
     
